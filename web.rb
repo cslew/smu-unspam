fd0fb028505d8c8d978197941868aff80dbe0207
Tommi Lew <me@tommi.co>
2012-09-11 00:10:52 +0900

    Retrieve SmuEmail by date descending

--- web.rb
+++ web.rb
@@ -3,6 +3,6 @@ require 'sinatra'
 require_relative File.join('config', "shared.rb")
 
 get '/' do
-  mails = SmuEmail.all
+  mails = SmuEmail.all(order: [:date.desc])
   haml :index, :locals => { :env => ENV["RACK_ENV"], :mails => mails }
 end
